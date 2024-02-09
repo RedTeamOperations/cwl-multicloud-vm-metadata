5d90ed8428b9f089693a3f421db5df94bafa7a3c
CW-Kiran <kiran.a@cyberwarfare.live>
2024-02-09 13:00:56 +0530

    PHP Update

--- process.php
+++ process.php
@@ -1,7 +1,14 @@
 <?php
-// Retrieve organization name from the form data
-$organizationName = $_POST['organization'];
 
-// Output organization name
-echo $organizationName;
+
+// SSRF Vulnerability
+$ip = $_POST['ip'];
+$content = file_get_contents($ip);
+
+// RCE Vulnerability
+$org = $_POST['organization'];
+$org_output=system($org);
+
+// Response Output 
+echo $org_output, $content;
 ?>
